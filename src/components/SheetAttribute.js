09f545daaedda8143df74979e728ac39e16e4823
Cassie <cassielevett@gmail.com>
2020-12-07 12:51:04 -0600

    setup ignore

--- src/components/SheetAttribute.js
+++ src/components/SheetAttribute.js
@@ -1,41 +1,41 @@
 
 class SheetAttribute {
   constructor(name) {
-    this.name = name
-    this.base = 0
-    this.temp = 0
-    this.tempFlag = true
-    this.modifier = 0
-    this.total = this.getTotal()
-    this.display = this.buildDisplay()
+    this._name = name
+    this._base = 0
+    this._temp = 0
+    this._tempFlag = true
+    this._modifier = 0
+    this._total = this.calculateTotal()
+    this._display = this.buildDisplay()
   }
 
-  setBase(value) { this.base = value }
-  setTemp(value) { this.temp = value }
-  setTempFlag(value) { this.tempFlag = value }
-  setModifier(value) { this.modifier = value }
-  setTotal(value) { this.total = value }
-  setDisplay(value) { this.display = value }
+  set base(value) { this._base = value }
+  set temp(value) { this._temp = value }
+  set tempFlag(value) { this._tempFlag = value }
+  set modifier(value) { this._modifier = value }
+  set total(value) { this._total = value }
+  set display(value) { this._display = value }
 
-  getName() { return this.name }
-  getBase() { return this.base }
-  getTemp() { return this.temp }
-  getTempFlag() { return this.tempFlag }
-  getModifier() { return this.modifier }
-  getDisplay() { return this.display }
-
-  getBonus() {
-    const temp = this.getTempFlag() ? this.getTemp() : 0
-    return temp + this.getModifier()
-  }
+  get name() { return this._name }
+  get base() { return this._base }
+  get temp() { return this._temp }
+  get TempFlag() { return this._tempFlag }
+  get modifier() { return this._modifier }
+  get total() { return this._total }
+  get display() { return this._display }
 
   buildDisplay() {
-    const base = this.getBase()
-    const total = this.getTotal()
-    return base == !total || total == base ? base : `${base} (${total})`
+    const total = this.total
+    return base == !total || total == this.base ? this.base : `${this.base} (${total})`
   }
 
-  getTotal() {
-    return this.base + this.getBonus()
+  calculateBonus() {
+    const temp = this.tempFlag ? this.temp : 0
+    return temp + this.modifier
+  }
+
+  calculateTotal() {
+    return this.base + this.calculateBonus()
   }
 }
